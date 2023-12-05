1e78909a8bed4c1cac22f7091a7a0d1632bff187
Théo Dubuisson <dubuissontheo@outlook.fr>
2023-12-05 14:26:55 +0100

    add calculate_score tests

--- test.py
+++ test.py
@@ -1,3 +1,36 @@
 import unittest
-from main import *
+from YamsGame import *
 
+
+class TestYamsGame(unittest.TestCase):
+    def test_brelan(self):
+        game = YamsGame([[1, 1, 1, 2, 3]])
+        self.assertEqual(game.calculate_score(), 28)
+
+    def test_carre(self):
+        game = YamsGame([[4, 4, 4, 4, 2]])
+        self.assertEqual(game.calculate_score(), 35)
+
+    def test_full(self):
+        game = YamsGame([[3, 3, 3, 2, 2]])
+        self.assertEqual(game.calculate_score(), 30)
+
+    def test_grande_suite(self):
+        game = YamsGame([[2, 3, 4, 5, 6]])
+        self.assertEqual(game.calculate_score(), 40)
+
+    def test_yams(self):
+        game = YamsGame([[6, 6, 6, 6, 6]])
+        self.assertEqual(game.calculate_score(), 50)
+
+    def test_chance(self):
+        game = YamsGame([[1, 1, 2, 2, 3]])
+        self.assertEqual(game.calculate_score(), 9)
+
+    def test_multiple_rolls(self):
+        game = YamsGame([[1, 1, 1, 2, 3], [4, 4, 4, 4, 2], [3, 3, 3, 2, 2]])
+        self.assertEqual(game.calculate_score(), 93)
+
+
+if __name__ == '__main__':
+    unittest.main()
