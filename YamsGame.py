95bb94a919d453b7670e14d543637739c20fe0d9
Théo Dubuisson <dubuissontheo@outlook.fr>
2023-12-05 14:53:33 +0100

    add get_roll_score.py to shorten calculate_score

--- YamsGame.py
+++ YamsGame.py
@@ -1,5 +1,4 @@
-from is_figure import *
-from collections import Counter
+from get_roll_score import *
 class YamsGame:
     def __init__(self, dice_rolls):
         self.dice_rolls = dice_rolls
@@ -9,28 +8,6 @@ class YamsGame:
         final_score = 0
 
         for roll in self.dice_rolls:
-            figures_obtained = []
-
-            if is_brelan(Counter(roll)):
-                figures_obtained.append(28)
-
-            if is_carre(Counter(roll)):
-                figures_obtained.append(35)
-
-            if is_full(Counter(roll)):
-                figures_obtained.append(30)
-
-            if is_grande_suite(sorted(roll)):
-                figures_obtained.append(40)
-
-            if is_yams(Counter(roll)):
-                figures_obtained.append(50)
-
-            if figures_obtained:
-                best_score_roll = max(figures_obtained)
-            else:
-                best_score_roll = sum(roll)
-
-            final_score += best_score_roll
+            final_score += get_roll_score(roll)
 
         return final_score
---
+++ get_roll_score.py
@@ -0,0 +1,28 @@
+from is_figure import *
+from collections import Counter
+
+
+def get_roll_score(roll):
+    figures_obtained = []
+
+    if is_brelan(Counter(roll)):
+        figures_obtained.append(28)
+
+    if is_carre(Counter(roll)):
+        figures_obtained.append(35)
+
+    if is_full(Counter(roll)):
+        figures_obtained.append(30)
+
+    if is_grande_suite(sorted(roll)):
+        figures_obtained.append(40)
+
+    if is_yams(Counter(roll)):
+        figures_obtained.append(50)
+
+    if figures_obtained:
+        best_score_roll = max(figures_obtained)
+    else:
+        best_score_roll = sum(roll)
+
+    return best_score_roll
