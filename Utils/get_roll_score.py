2f8bbd56fbaae0a7ccb6eee5ad6b462e2dea15f0
Théo Dubuisson <dubuissontheo@outlook.fr>
2023-12-05 15:04:26 +0100

    renaming figures_obtained var by figures_score_obtained

--- Utils/get_roll_score.py
+++ Utils/get_roll_score.py
@@ -3,25 +3,25 @@ from collections import Counter
 
 
 def get_roll_score(roll):
-    figures_obtained = []
+    figures_score_obtained = []
 
     if is_brelan(Counter(roll)):
-        figures_obtained.append(28)
+        figures_score_obtained.append(28)
 
     if is_carre(Counter(roll)):
-        figures_obtained.append(35)
+        figures_score_obtained.append(35)
 
     if is_full(Counter(roll)):
-        figures_obtained.append(30)
+        figures_score_obtained.append(30)
 
     if is_grande_suite(sorted(roll)):
-        figures_obtained.append(40)
+        figures_score_obtained.append(40)
 
     if is_yams(Counter(roll)):
-        figures_obtained.append(50)
+        figures_score_obtained.append(50)
 
-    if figures_obtained:
-        best_score_roll = max(figures_obtained)
+    if figures_score_obtained:
+        best_score_roll = max(figures_score_obtained)
     else:
         best_score_roll = sum(roll)
 
--- YamsGame.py
+++ YamsGame.py
@@ -1,10 +1,11 @@
 from Utils.get_roll_score import *
+
+
 class YamsGame:
     def __init__(self, dice_rolls):
         self.dice_rolls = dice_rolls
 
     def calculate_score(self):
-
         final_score = 0
 
         for roll in self.dice_rolls:
